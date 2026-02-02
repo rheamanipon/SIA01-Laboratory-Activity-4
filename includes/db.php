7e1f54ee76f6e9d17e1e99ac8937af4cf0132972
aintmchk <maniponrhea1@gmail.com>
2026-02-03 03:15:35 +0800

    first commit

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,10 @@
 <?php
 $db["db_host"] = "localhost";
 $db["db_user"] = "root";
-$db["db_pass"] = "";
+$db["db_pass"] = " ";
 $db["db_name"] = "sia01_lab_cms_manipon";
+$db["db_name"] = "cms";
+
 
 foreach ($db as $key => $value) {
     define(strtoupper($key), $value);
